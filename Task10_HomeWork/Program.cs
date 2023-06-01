d3e95522134503eb521039a54ca69ceee950dcf5
TimofeyKornilin <you@example.com>
2023-06-01 12:20:37 +0700

    Program find second digit from three digits number

---
+++ Task10_HomeWork/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 10: Напишите программу,
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("   *** Start program ***");
+Console.WriteLine("");
+Console.WriteLine("");
+
+Console.Write("Enter three digits number:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+
+if (number < 100 || number > 999) Console.WriteLine("Entered not three digit number. Try again");
+else
+{
+    int findDigit = SecondDigit(number);
+    Console.WriteLine($"Second digit is {findDigit}");
+}
+
+int SecondDigit(int num)
+{
+    int result = (num / 10) % 10;
+    return result;
+}
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("    *** END program ***");
+Console.WriteLine("");
